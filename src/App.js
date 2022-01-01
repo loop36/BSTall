aa25f998de2b9932108f9a5f724ee17abd44201a
naveenvb560 <naveenvb560@gmail.com>
2022-01-02 00:57:14 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/Layout/Header";
+import { Router, Routes } from "react-router";
 
 function App() {
 
-  return ( <h1>Book App</h1>
+  return ( 
+
+  <Header />
 
   );
 }
