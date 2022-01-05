7a84c38daf9b5e94d2dc68b36d94f73622213b07
unknown <anandvb2013@gmail.com>
2022-01-06 00:44:46 +0530

    Added Footer

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='flex justify-center w-screen h-11 bg-gray-700 '>
+            <p className=' text-gray-50 m-auto capitalize'> created with love and Coffee<strong className='font-bold'> CodeLoop Inc.</strong></p>
+        </div>
+    )
+}
+
+export default Footer
